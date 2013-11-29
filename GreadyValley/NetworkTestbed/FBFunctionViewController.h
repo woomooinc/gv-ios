647f13f5409a02a849906f0aff311a1c9510f87d
abyssband <abyssband@gmail.com>
2013-11-29 02:55:57 -0800

    Added: Facebook SDK Test unit

---
+++ GreadyValley/NetworkTestbed/FBFunctionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBFunctionViewController.h
+//  GreadyValley
+//
+//  Created by Abyss on 11/29/13.
+//  Copyright (c) 2013 woomoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBFunctionViewController : UIViewController
+
+@end
---
+++ GreadyValley/NetworkTestbed/FBFunctionViewController.m
@@ -0,0 +1,38 @@
+//
+//  FBFunctionViewController.m
+//  GreadyValley
+//
+//  Created by Abyss on 11/29/13.
+//  Copyright (c) 2013 woomoo. All rights reserved.
+//
+
+#import "FBFunctionViewController.h"
+
+@interface FBFunctionViewController ()
+
+@end
+
+@implementation FBFunctionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
